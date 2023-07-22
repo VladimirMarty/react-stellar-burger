8ba3f49ddac9bf91198de0a27045f673c6ffa84d
Vladimir Martirosyan <mart-van9@yandex.ru>
2023-07-22 17:07:55 +0300

    order

--- src/components/orderDetails/orderDetails.jsx
+++ src/components/orderDetails/orderDetails.jsx
@@ -1,15 +1,16 @@
 function OrderDetails(orderNumber) {
-    const orderNumber = '034536';
- return <form>
-<div>
-      <h1 >{orderNumber}</h1>
-      <p>идентификатор заказа</p>
-      <img  alt="Заказ подтверждён"/>
-      <p >Ваш заказ начали готовить</p>
-      <p >Дождитесь готовности на орбитальной станции</p>
-    </div>
-
-  </form>;
+  const orderNumber = "034536";
+  return (
+    <form>
+      <div>
+        <h1>{orderNumber}</h1>
+        <p>идентификатор заказа</p>
+        <img alt="Заказ подтверждён" />
+        <p>Ваш заказ начали готовить</p>
+        <p>Дождитесь готовности на орбитальной станции</p>
+      </div>
+    </form>
+  );
 }
 
 export default OrderDetails;
